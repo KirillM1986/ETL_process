ec67ba2cc832b720c47fec0597afe1e690db30d3
KirillM1986 <kirillmajorov4973@gmail.com>
2025-06-29 19:15:39 +0400

    Add files via upload

---
+++ etl.py
@@ -0,0 +1,187 @@
+import time
+
+import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine
+import re
+import os
+import chardet
+import datetime
+import logging
+
+logging.basicConfig(filename='app.log',
+                    level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s'
+                    )
+
+
+# Логирование в БД процесса ETL
+def insert_log_data(start_reader,
+                    start_loading_time,
+                    end_loading_time,
+                    file_name,
+                    log_file):
+    try:
+        conn = psycopg2.connect(
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD"),
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT")
+        )
+
+        cur = conn.cursor()
+
+        cur.execute("""
+                INSERT INTO LOGS.ETL_LOGS (
+                    start_reader,
+                    start_loading_time,
+                    end_loading_time,
+                    file_name,
+                    log_file
+                ) VALUES (%s, %s, %s, %s, %s)""",
+                    (start_reader,
+                    start_loading_time,
+                    end_loading_time,
+                    file_name,
+                    log_file))
+
+        conn.commit()
+
+    except psycopg2.Error as e:
+        logging.error(f"Ошибка при записи в таблицу LOGS.ETL_LOGS: {e}")
+        if conn:
+            conn.rollback()
+        raise
+    finally:
+        if conn:
+            conn.close()
+
+
+# Преобразует дату в формат ГГГГ-ММ-ДД
+def format_date(date_string):
+    patterns = [
+        (r'\d{2}\.\d{2}\.\d{4}', lambda x: re.sub(r'(\d{2})\.(\d{2})\.(\d{4})', r'\3-\2-\1', x)),
+        (r'\d{4}\.\d{2}\.\d{2}', lambda x: x.replace('.', '-')),
+        (r'\d{4}-\d{2}-\d{2}', lambda x: x),
+        (r'\d{2}-\d{2}-\d{4}', lambda x: re.sub(r'(\d{2})-(\d{2})-(\d{4})', r'\3-\2-\1', x))
+    ]
+
+    for pattern, transform in patterns:
+        if re.match(pattern, date_string):
+            return transform(date_string)
+    logging.error(f"Некорректный формат даты: {date_string}")
+    raise ValueError (f"Некорректный формат даты: {date_string}")
+
+
+# Форматирование дат в DataFrame
+def process_dataframe(reader, columns_to_format):
+    try:
+        for column_idx in columns_to_format:
+            if column_idx < len(reader.columns):
+                reader.iloc[:, column_idx] = reader.iloc[:, column_idx].apply(format_date)
+        return reader
+    except Exception as e:
+        logging.error(f"Ошибка при обработке DataFrame: {str(e)}")
+        raise
+
+
+# ETL данных файлов
+def process_files(directory_path, engine):
+    start_reader = None
+    start_loading_time = None
+    end_loading_time = None
+    file_processors = {
+        'ft_balance_f.csv': lambda df: process_dataframe(df, [0]),
+        'ft_posting_f.csv': lambda df: process_dataframe(df, [0]),
+        'md_account_d.csv': lambda df: process_dataframe(df, [0, 1]),
+        'md_currency_d.csv': lambda df: process_dataframe(df, [1, 2]),
+        'md_exchange_rate_d.csv': lambda df: process_dataframe(df, [0, 1]),
+        'md_ledger_account_s.csv': lambda df: process_dataframe(df, [10, 11])
+    }
+
+    # Извлесение данных из файлов
+    for filename in os.listdir(directory_path):
+        if filename not in file_processors:
+            logging.error(f'Нет файлов для загрузки')
+            continue
+
+        file_path = os.path.join(directory_path, filename)
+
+        start_reader = datetime.datetime.now()
+        try:
+            with open(file_path, 'rb') as f:
+                result_encoding = chardet.detect(f.read())['encoding']
+            reader = pd.read_csv(file_path,
+                                sep=';',
+                                encoding=result_encoding,
+                                dtype='str')
+
+            processor = file_processors[filename]
+            # трасформация данных
+            processed_df = processor(reader)
+
+            processed_df.columns = [col.lower() for col in processed_df.columns]
+
+            # Загрузка данных в БД
+            start_loading_time = datetime.datetime.now()
+            time.sleep(5)
+            processed_df.to_sql(
+                filename.replace('.csv', ''),
+                engine,
+                schema='ds',
+                if_exists='replace',
+                index=False,
+                method='multi',
+                chunksize=10000
+            )
+            end_loading_time = datetime.datetime.now()
+
+            insert_log_data(start_reader,
+                            start_loading_time,
+                            end_loading_time,
+                            filename,
+                            f'Данные успешно загружены')
+
+            logging.info(f'Данные файла {filename} успешно загружены в БД')
+
+        except Exception as e:
+            insert_log_data(start_reader,
+                                start_loading_time,
+                                end_loading_time,
+                                filename,
+                                f'Ошибка при обработке файла {filename}: {str(e)}')
+            logging.error(f'Ошибка при обработке файла {filename}: {str(e)}')
+            raise
+
+
+# Подключение к БД
+def database_connection():
+    try:
+        engine = create_engine(
+            f'postgresql://{os.getenv("DB_USER")}:'
+            f'{os.getenv("DB_PASSWORD")}@'
+            f'{os.getenv("DB_HOST")}:'
+            f'{os.getenv("DB_PORT")}/'
+            f'{os.getenv("DB_NAME")}'
+        )
+
+        return engine
+    except Exception as e:
+        logging.error(f"Ошибка подключения к БД: {str(e)}")
+        raise
+
+
+# Функция запуска ETL
+def main():
+    try:
+        engine = database_connection()
+        process_files(os.getenv("DIRECTORY_PATH"), engine)
+    except Exception as e:
+        logging.error(f"Критическая ошибка: {str(e)}")
+        raise
+
+if __name__ == "__main__":
+    main()
+
+
