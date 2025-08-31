81bc8be503cd2e1f9255c6d23a9320ee43bbc34b
KirillM1986 <kirillmajorov4973@gmail.com>
2025-08-31 16:15:10 +0400

    Add files via upload

---
+++ Проект_2/Проект_2_1_версия_2.sql
@@ -0,0 +1,15 @@
+-- Создать временнную таблицу с уникальными значениями
+SELECT DISTINCT *
+INTO temptable_client
+FROM dm.client;
+
+-- Очистить таблицу dm.client
+DELETE FROM dm.client;
+
+-- Наполнить уникальными значениями из временной таблицы таблицу dm.client
+INSERT INTO dm.client
+SELECT * FROM temptable_client;
+
+-- Удалить временную таблицу
+DROP TABLE temptable_client;
+
